b7de2521a2b593ccb00a016cefba5b2ee74947ec
Ryan Holt <ryholtan51@gmail.com>
2023-01-04 14:27:36 -0700

    first assignment James Bond

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
+
+
+        // string play = "y";
+
+        // while (play == "y")
+        // {
+        //     Console.Write("What is your first name? ");
+        //     string first = Console.ReadLine();
+        //     Console.Write("What is your last name? ");
+        //     string last = Console.ReadLine();
+
+        //     Console.WriteLine($"Your name is {last}, {first} {last}.");
+
+        //     Console.Write("Would you like to play again? (y/n) ");
+        //     play = Console.ReadLine();
+        // }
+        
+
+
     }
 }
