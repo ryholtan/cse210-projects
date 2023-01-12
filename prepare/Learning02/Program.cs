c1748ddd1dbac1ad8a0cdda5dfccfc7791a78820
Ryan Holt <ryholtan51@gmail.com>
2023-01-11 22:56:44 -0700

    fun with classes, resumes, and jobs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume res1 = new Resume();
+
+        res1._name = "Allison Rose";
+        res1._jobList.Add(job1);
+        res1._jobList.Add(job2);
+
+        res1.Display();
     }
 }
