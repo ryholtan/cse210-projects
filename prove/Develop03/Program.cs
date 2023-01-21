296c7ab57c0d0191cf32cf186a71abb58acb0f62
Ryan Holt <ryholtan51@gmail.com>
2023-01-21 00:46:41 -0700

    created scripturememorizer

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference scriptureReference = new Reference("Proverbs", "3", "5-6");
+        Scripture scripture = new Scripture(scriptureReference, "Trust in the Lord with all thine " +
+            "heart and lean not to thine own understanding; in all thy ways acknowledge him, and " +
+            "he shall direct thy paths.");
+        ScriptureMemorizer scriptureMemorizer = new ScriptureMemorizer(scripture);
+
+        string userInput = "";
+
+        while (userInput != "quit" && scriptureMemorizer.hasWordsLeft() == true)
+        {
+            Console.Clear();
+            Console.WriteLine(string.Format("{0} {1}", scriptureReference.toString(), scriptureMemorizer.toString()));
+            Console.WriteLine();
+            Console.ReadLine();
+            scriptureMemorizer.removeWordsFromText();
+        }
     }
 }
